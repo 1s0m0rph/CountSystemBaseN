51c1ed2619a8f0247f6eada6588578f5d16e962c
Daniel Taylor <32105556+Ambulator@users.noreply.github.com>
2017-11-13 18:55:16 -0700

    Update TimedCount.java

--- TimedCount.java
+++ TimedCount.java
@@ -1,23 +1,20 @@
 
 /**
+ * A testing method for the BaseN system. Tests efficiency of certain algorithms (addition, with the current implementation)
  * 
- * 
- * @author Daniel Taylor (DTIII)
- * @version .._-
+ * @author Daniel Taylor (Ambulator)
+ * @version 13.11.17
  */
 public class TimedCount
 {
     public static void main(String[] args)
     {
         System.out.print("\f");
-        //         int iterations = 128000;
-        //         int sleeptime = 1;
         Decimal d = new Decimal();
         String count = "0", base = "999999";
         String powNum = d.decrement(d.pow("10","10"));
         while(d.lessThan(base,"1000000"))
         {
-            //String sum = "0";
             long sum = 0;
             BaseN addNum = new BaseN(base);
             addNum.convert(powNum);
@@ -32,18 +29,13 @@ public class TimedCount
                 {
                     num.add(addNum);
                     c++;
-                    //num.increment();
                 }
                 
-                //num.print();
-                //sum = d.add(sum,num.convertTo_10());
                 sum += c;
             }
             System.out.println("base n_" + base + ":\t" + sum);
             count = d.increment(count);
             base = d.increment(base);
-            // this process proves that n base 9 is 52% more efficient than just base 10, and is, in fact the most efficient base
-            //however, even n_9 is 82% less efficient on average than just incrementing a long
         }
     }
 }
